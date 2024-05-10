b2e4ccae84b91e9f9131b3bd3b849c86ae325a40
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-05-10 22:03:24 +0530

    Express

---
+++ Express/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const  app = express()
+
+let port = 8080
+
+app.listen(port, () => {
+    console.log(`Port is listening ${port}`)
+})
+
+app.get("/",(req, res) =>  {
+    res.send("Basic Response")
+    console.log("request recieved")
+})
+app.get("/apple", (req, res) => {
+    let code = "<h1>Apple<h1/><ul><li>Red</li><li>Sweet</li></ul>"
+    res.send(code);
+});
+app.get("/Orange",(req, res) =>  {
+    let code = "<h1>Orange<h1/><ul><li>Orange</li><li>Sweet and Salty</li></ul>"
+    res.send("Basic Response")
+})
+app.get("*",(req, res) =>  {
+    res.send("There is no Response")
+})
+
